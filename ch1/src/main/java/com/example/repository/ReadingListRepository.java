7f5d3e4d82f5f6a55e82e5f43c08bcf1c7ab1e29
Shin seongsu <tlstjdtn321@naver.com>
2020-11-25 20:51:38 +0900

    ReadingList repository

--- ch1/src/main/java/com/example/repository/ReadingListRepository.java
+++ ch1/src/main/java/com/example/repository/ReadingListRepository.java
@@ -1,10 +1,11 @@
 package com.example.repository;
 
 import com.example.domain.Book;
+import com.example.domain.Reader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ReadingListRepository extends JpaRepository<Book, Long> {
-    List<Book> findByReader(String reader);
+    List<Book> findByReader(Reader reader);
 }
