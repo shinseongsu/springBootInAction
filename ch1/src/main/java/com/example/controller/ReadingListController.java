7bbafc522968f9ce1d26726bb5bce816157a4088
Shin seongsu <tlstjdtn321@naver.com>
2020-11-25 20:49:28 +0900

    amazonProperties 추가

--- ch1/src/main/java/com/example/controller/ReadingListController.java
+++ ch1/src/main/java/com/example/controller/ReadingListController.java
@@ -1,8 +1,11 @@
 package com.example.controller;
 
 import com.example.domain.Book;
+import com.example.domain.Reader;
+import com.example.properties.AmazonProperties;
 import com.example.repository.ReadingListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,28 +19,31 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/")
+@ConfigurationProperties(prefix="amazon")   // 프로퍼티 주입
 public class ReadingListController {
-
-    private static final String reader = "craig";
-
     private ReadingListRepository readingListRepository;
+    private AmazonProperties amazonProperties;
 
     @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository) {
+    public ReadingListController(ReadingListRepository readingListRepository
+                                ,AmazonProperties amazonProperties ) {          // AmazonProperties 주입
         this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
     }
 
     @RequestMapping( method = RequestMethod.GET)
-    public String readerBooks(Model model) {
+    public String readerBooks(Reader reader, Model model) {
         List<Book> readingList = readingListRepository.findByReader(reader);
         if(readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateId());
         }
         return "readingList";
     }
 
     @RequestMapping( method = RequestMethod.POST)
-    public String addToReadingList(Book book) {
+    public String addToReadingList(Reader reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
 
