0dbc8b5cedc959558fe336d6924c082c697b0f87
Shin seongsu <tlstjdtn321@naver.com>
2020-11-30 19:07:17 +0900

    serviceImpl 추가

---
+++ ch3/src/main/java/com/example/serviceImpl/BoardServiceImpl.java
@@ -0,0 +1,46 @@
+package com.example.serviceImpl;
+
+import com.example.domain.BoardVO;
+import com.example.service.BoardService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    @Override
+    public String hello(String name) {
+        return "hello: " + name;
+    }
+
+    @Override
+    public BoardVO getBoard() {
+        BoardVO board = new BoardVO();
+        board.setSeq(1);
+        board.setTitle("테스트 제목....");
+        board.setWriter("테스트");
+        board.setContent("테스트 내용입니다................");
+        board.setCreateDate(new Date());
+        board.setCnt(0);
+        return board;
+    }
+
+    @Override
+    public List<BoardVO> getBoardList() {
+        List<BoardVO> boardList = new ArrayList<BoardVO>();
+        for(int i = 1 ; i <= 10; i++) {
+            BoardVO board = new BoardVO();
+            board.setSeq(1);
+            board.setTitle("제목" + i);
+            board.setWriter("테스터");
+            board.setContent(i + "번 내용입니다.");
+            board.setCreateDate(new Date());
+            board.setCnt(0);
+            boardList.add(board);
+        }
+        return boardList;
+    }
+}
