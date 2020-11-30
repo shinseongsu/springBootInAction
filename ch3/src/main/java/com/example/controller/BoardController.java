29692c85506feca20519ee23f1ea1e1b67d4ba87
Shin seongsu <tlstjdtn321@naver.com>
2020-11-30 19:05:23 +0900

    restConroller 컨트롤러 내용추가

--- ch3/src/main/java/com/example/controller/BoardController.java
+++ ch3/src/main/java/com/example/controller/BoardController.java
@@ -1,17 +1,34 @@
 package com.example.controller;
 
+import com.example.domain.BoardVO;
+import com.example.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @RestController
 public class BoardController {
 
-    public BoardController() {
-        System.out.println("===> BoardController 생성");
-    }
+    @Autowired
+    private BoardService boardService;
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return "hello: " + name;
+        return boardService.hello(name);
     }
+
+    @GetMapping("/getBoard")
+    public BoardVO getBoard() {
+        return boardService.getBoard();
+    }
+
+    @GetMapping("/getBoardList")
+    public List<BoardVO> getBoardList() {
+        return boardService.getBoardList();
+    }
+
 }
