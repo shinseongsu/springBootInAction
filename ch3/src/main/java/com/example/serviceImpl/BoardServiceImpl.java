0100b3371eaa74d665500bd8ae66f6326791de7b
Shin seongsu <tlstjdtn321@naver.com>
2020-12-02 22:52:41 +0900

    타입 변경으로 값 수정

--- ch3/src/main/java/com/example/serviceImpl/BoardServiceImpl.java
+++ ch3/src/main/java/com/example/serviceImpl/BoardServiceImpl.java
@@ -19,7 +19,7 @@ public class BoardServiceImpl implements BoardService {
     @Override
     public BoardVO getBoard() {
         BoardVO board = new BoardVO();
-        board.setSeq(1);
+        board.setSeq(1L);
         board.setTitle("테스트 제목....");
         board.setWriter("테스트");
         board.setContent("테스트 내용입니다................");
@@ -33,7 +33,7 @@ public class BoardServiceImpl implements BoardService {
         List<BoardVO> boardList = new ArrayList<BoardVO>();
         for(int i = 1 ; i <= 10; i++) {
             BoardVO board = new BoardVO();
-            board.setSeq(1);
+            board.setSeq(1L);
             board.setTitle("제목" + i);
             board.setWriter("테스터");
             board.setContent(i + "번 내용입니다.");
