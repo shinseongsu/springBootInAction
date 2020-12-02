dc5927b69f03b34352476722f01591f6c8b2118b
Shin seongsu <tlstjdtn321@naver.com>
2020-12-02 22:51:56 +0900

    jpa 연습을 위한 추가

--- ch3/src/main/java/com/example/repository/BoardRepository.java
+++ ch3/src/main/java/com/example/repository/BoardRepository.java
@@ -1,7 +1,22 @@
 package com.example.repository;
 
 import com.example.domain.BoardVO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BoardRepository extends CrudRepository<BoardVO, Long> {
+    List<BoardVO> findByTitle(String searchKeyword);
+
+    List<BoardVO> findByContentContaining(String searchKeyword);
+
+    List<BoardVO> findByTitleContainingOrContentContaining(String title, String content);
+
+    List<BoardVO> findByTitleContainingOrderBySeqDesc(String searchKeyword);
+
+//    List<BoardVO> findByTitleContaining(String searchKeyword, Pageable paging);
+
+    Page<BoardVO> findByTitleContaining(String searchKeyword, Pageable paging);
 }
