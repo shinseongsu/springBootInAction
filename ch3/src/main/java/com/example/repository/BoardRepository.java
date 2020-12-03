fbcf450d6ca9edee5cd391cc3a6f2017c4cdb031
Shin seongsu <tlstjdtn321@naver.com>
2020-12-03 22:10:46 +0900

    @Query 어노테이션 추가

--- ch3/src/main/java/com/example/repository/BoardRepository.java
+++ ch3/src/main/java/com/example/repository/BoardRepository.java
@@ -3,7 +3,9 @@ package com.example.repository;
 import com.example.domain.BoardVO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -19,4 +21,27 @@ public interface BoardRepository extends CrudRepository<BoardVO, Long> {
 //    List<BoardVO> findByTitleContaining(String searchKeyword, Pageable paging);
 
     Page<BoardVO> findByTitleContaining(String searchKeyword, Pageable paging);
+
+    @Query("SELECT b FROM BoardVO b where b.title like %?1% ORDER BY b.seq DESC")
+    List<BoardVO> queryAnnotationTest1(String searchKeyword);
+
+    @Query("SELECT b FROM BoardVO b "
+         + "WHERE b.title like %:searchKeyword% "
+         + "ORDER BY b.seq DESC")
+    List<BoardVO> queryAnnotationTest2(@Param("searchKeyword") String searchKeyword);
+
+    @Query("SELECT b.seq, b.title, b.writer, b.createDate "
+         + "FROM BoardVO b "
+         + "WHERE b.title like %?1%"
+         + "ORDER BY b.seq DESC")
+    List<Object[]> queryAnnotationTest3(@Param("searchKeyword") String searchKeyword);
+
+    @Query(value = "select seq, title, writer, create_date "
+                 + "from boardvo where title like '%'||?1||'%' "
+                 + "order by seq desc", nativeQuery = true)
+    List<Object[]> queryAnnotationTest4(String searchKeyword);
+
+    @Query("SELECT b FROM BoardVO b ORDER BY b.seq DESC")
+    List<BoardVO> queryAnnotationTest5(Pageable paging);
+
 }
