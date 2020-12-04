7bf5d66f4b577ee4baede3db2033d9fadb600980
Shin seongsu <tlstjdtn321@naver.com>
2020-12-04 19:09:21 +0900

    MemberVO 추가

---
+++ ch3/src/main/java/com/example/domain/MemberVO.java
@@ -0,0 +1,27 @@
+package com.example.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@ToString(exclude = "boardList")
+@Entity
+public class MemberVO {
+
+    @Id
+    @Column(name = "MEMBER_ID")
+    private String id;
+    private String password;
+    private String name;
+    private String role;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
+    private List<BoardVO> boardList = new ArrayList<BoardVO>();
+
+}
