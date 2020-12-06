e8732f0eb2cf1a5e3bf4a437a6eabe6a1bd0b3af
Shin seongsu <tlstjdtn321@naver.com>
2020-12-06 15:15:55 +0900

    수정 컨트롤러 추가

--- ch3/src/main/java/com/example/controller/BoardViewController.java
+++ ch3/src/main/java/com/example/controller/BoardViewController.java
@@ -1,13 +1,18 @@
 package com.example.controller;
 
 import com.example.domain.BoardVO;
+import com.example.domain.MemberVO;
+import com.example.repository.MemberRepository;
 import com.example.service.BoardViewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -15,6 +20,9 @@ public class BoardViewController {
 
     @Autowired
     private BoardViewService boardService;
+
+    @Autowired
+    private MemberRepository memberRepo;
 
     @RequestMapping("/getBoardList")
     public String getBoardList(Model model, BoardVO board) {
@@ -36,5 +44,35 @@ public class BoardViewController {
         return "getBoardList";
     }
 
+    @GetMapping("/insertBoard")
+    public String insertBoardView() {
+        return "insertBoard";
+    }
+
+    @PostMapping("/insertBoard")
+    public String insertBoard(BoardVO board) {
+        MemberVO member = memberRepo.findById("member1").get();
+        board.setMember(member);
+        boardService.insertBoard(board);
+        return "redirect:getBoardList";
+    }
+
+    @GetMapping("/getBoard")
+    public String getBoard(BoardVO board, Model model) {
+        model.addAttribute("board", boardService.getBoard(board));
+        return "getBoard";
+    }
+
+    @PostMapping("/updateBoard")
+    public String updateBoard(BoardVO board) {
+        boardService.updateBoard(board);
+        return "forward:getBoardList";
+    }
+
+    @GetMapping("/deleteBoard")
+    public String deleteBoard(BoardVO board) {
+        boardService.deleteBoard(board);
+        return "forward:getBoardList";
+    }
 
 }
