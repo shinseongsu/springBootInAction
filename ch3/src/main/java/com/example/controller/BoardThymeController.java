4a80c773f856a9e9458647b2d4ab60a134976c61
Shin seongsu <tlstjdtn321@naver.com>
2020-12-06 15:15:29 +0900

    thymeleaf 템플릿 컨트롤러 추가

---
+++ ch3/src/main/java/com/example/controller/BoardThymeController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BoardThymeController {
+
+    @GetMapping("/hello")
+    public void hello(Model model) {
+        model.addAttribute("greeting", "Hello 타임리프.^^");
+    }
+
+}
